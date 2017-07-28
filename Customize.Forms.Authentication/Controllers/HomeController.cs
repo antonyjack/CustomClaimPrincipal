c688df9f407b3333bd2edd7999dd7ffc3274045c
unknown <Antony_S@ravsoftw0252.MEDUSA.LOCAL>
2017-07-28 15:27:03 +0530

    Custom claim principal implementation

---
+++ Customize.Forms.Authentication/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Customize.Forms.Authentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Customize.Forms.Authentication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ClaimsPrincipal principal = User as ClaimsPrincipal;
+            string Name = principal.Claims.Where(x => x.Type == ClaimTypes.Name).Select(x => x.Value).FirstOrDefault();
+            string FullName = principal.Claims
+                .Where(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/fullname").Select(x => x.Value).FirstOrDefault();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Customize.Forms.Authentication/Models/CustomClaimPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace Customize.Forms.Authentication.Models
+{
+    public class CustomClaimPrincipal
+    {
+        private string Name = null;
+        public CustomClaimPrincipal(string name)
+        {
+            Name = name;
+        }
+
+        private List<Claim> GenerateClaimes()
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, Name));            
+            claims.Add(new Claim(ClaimTypes.Authentication, "true"));
+            claims.Add(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/fullname", "Full Name"));
+            return claims;
+        }
+
+        private ClaimsIdentity GenerateClaimesIndentity()
+        {
+            ClaimsIdentity identity = new ClaimsIdentity(GenerateClaimes(), "Forms");
+            return identity;
+        }
+
+        public ClaimsPrincipal GenerateClaimesPrincipal()
+        {
+            ClaimsPrincipal principal = new ClaimsPrincipal(GenerateClaimesIndentity());
+            return principal;
+        }
+    }
+}
